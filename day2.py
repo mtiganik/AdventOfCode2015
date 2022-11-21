b92d837f2a057b93b179f4db31d6df383522efa9
Mihkel Tiganik <mtiganik@gmail.com>
2022-11-21 23:43:45 +0200

    day2 part 2

--- day2.py
+++ day2.py
@@ -1,12 +1,28 @@
+def Day2P2(input):
+    total = 0
+    for x in f:
+        len, width, height = x.split("x")
+        box = [int(len), int(width), int(height)]
+        box.sort()
+        ribbon = 2*box[0] + 2*box[1]
+        bow = box[0]*box[1]*box[2]
+        area = bow + ribbon
+        print(area)
+        total = total + area
+    print(total)
+
+def Day2P1(input):
+    total= 0
+    for x in f:
+        len, width, height = x.split("x")
+        len, width, height = int(len), int(width), int(height)
+        box = [2*len*width, 2*width*height, 2* height*len]
+        box.sort()
+        area = box[0] + box[1]+box[2] + box[0]/2
+        print(area)
+        total = total + area
+    print(total)
+
 f = open("day2.txt")
-total= 0
-for x in f:
-    len, width, height = x.split("x")
-    len, width, height = int(len), int(width), int(height)
-    box = [2*len*width, 2*width*height, 2* height*len]
-    box.sort()
-    area = box[0] + box[1]+box[2] + box[0]/2
-    print(area)
-    total = total + area
-print(total)
-#input = f.readline()
+Day2P2(f)
+#Day2P1(f)
